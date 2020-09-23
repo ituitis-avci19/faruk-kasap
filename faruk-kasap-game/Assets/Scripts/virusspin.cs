d27e6adf1de94f3a0dc236a026e010331061d1d7
Halil-Ibrahim-Kasapoglu <36034222+Halil-Ibrahim-Kasapoglu@users.noreply.github.com>
2020-09-23 18:34:23 +0300

    many added
    ucuyor baba

---
+++ faruk-kasap-game/Assets/Scripts/virusspin.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class virusspin : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2 ScaleRange;
+    public float scaleSpeed;
+
+    public float rotateSpeed;
+
+    int scaleway = 1;
+    float scale = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y ,
+            (transform.eulerAngles.z + (rotateSpeed * Time.deltaTime)) % 360);
+
+        if (scaleway == 1)
+        {
+            scale += scaleSpeed * Time.deltaTime;
+            if (scale >= ScaleRange.y)
+            {
+                scale = ScaleRange.y;
+                scaleway = 2;
+            }
+        }else if (scaleway == 2)
+        {
+            scale -= scaleSpeed * Time.deltaTime;
+            if (scale <= ScaleRange.x)
+            {
+                scale = ScaleRange.x;
+                scaleway = 1;
+            }
+        }
+
+        transform.localScale = new Vector3(scale, scale, 1);
+    }
+}
