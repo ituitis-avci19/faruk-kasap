c388b6b1ed29a46718bb5fea5d858418120f0d34
Faruk Avcı <farukkastamonuda@gmail.com>
2020-09-23 18:37:20 +0300

    37-kastamonu
    61-trabzon

--- faruk-kasap-game/Assets/Scripts/SickPerson.cs
+++ faruk-kasap-game/Assets/Scripts/SickPerson.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SickPerson : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject sickperson;
     void Start()
     {
         
@@ -13,6 +14,13 @@ public class SickPerson : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "bullet")
+        {
+            Destroy(sickperson);
+        }
     }
 }
--- faruk-kasap-game/Assets/Scripts/movement.cs
+++ faruk-kasap-game/Assets/Scripts/movement.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject dayi,bullet;
+    public GameObject dayi,bullet,cross_symbol;
     public Joystick joystick_mov,joystick_fire;
-    public float speed,bullet_speed;
-
+    public float speed,bullet_speed,atis_mesafesi,kirlilik_artis_hizi,max_kirlilik;
+    public Image kirlilik_bari;
+    public Sprite[] prof_sprites;
+    public Color white,black;
+    public GameObject[] mask_array;
+    
+    
     private Rigidbody2D rigbody;
-    private int direction = 0,atisyapildi=0;//0 mean left 1 mean right
+    private int direction = 0, atisyapildi = 0,maske_var_mi=0;//0 mean left 1 mean right
     private Vector2 lastfirecoordinates;
+    private float kirlilik;
+ 
     void Start()
     {
+        maske_var_mi = 0;
+        kirlilik = 0;
         atisyapildi = direction = 0;
         rigbody = dayi.GetComponent<Rigidbody2D>();
     }
@@ -21,6 +31,30 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int kacibeyaz = maske_var_mi;
+        int kacisiyah = 3 - maske_var_mi;
+        for(int i = 0; i < kacibeyaz; i++)
+        {
+            mask_array[i].GetComponent<Image>().color = white;
+        }
+        for(int i = 2; i >= 3 - kacisiyah; i--)
+        {
+            mask_array[i].GetComponent<Image>().color = black;
+        }
+        if (maske_var_mi == 0)
+        {
+            dayi.GetComponent<SpriteRenderer>().sprite = prof_sprites[0];
+        }
+        else
+        {
+            dayi.GetComponent<SpriteRenderer>().sprite = prof_sprites[1];
+        }
+        kirlilik += kirlilik_artis_hizi*Time.deltaTime;
+        kirlilik_bari.fillAmount = kirlilik / max_kirlilik;
+        if (kirlilik >= max_kirlilik)
+        {
+            Debug.Log("GAME IS OVER!");
+        }
         rigbody.velocity = new Vector2(speed * joystick_mov.Horizontal, speed * joystick_mov.Vertical);
         if (joystick_mov.Horizontal > 0 && direction==0)
         {
@@ -34,8 +68,19 @@ public class movement : MonoBehaviour
         }
         if(joystick_fire.Horizontal!=0 || joystick_fire.Vertical != 0)
         {
+            cross_symbol.SetActive(true);
+            float x = joystick_fire.Horizontal;
+            float y = joystick_fire.Vertical;
+            float hipo = Mathf.Sqrt(x * x + y * y);
+            float ratio = atis_mesafesi / hipo;
+            //cross_symbol.transform.position = new Vector2(rigbody.position.x + atis_mesafesi * joystick_fire.Horizontal, rigbody.position.y + atis_mesafesi * joystick_fire.Vertical);
+            cross_symbol.transform.position = new Vector2(rigbody.position.x + ratio * x, rigbody.position.y + ratio * y);
             atisyapildi = 1;
             lastfirecoordinates = new Vector2(joystick_fire.Horizontal, joystick_fire.Vertical);
+        }
+        else
+        {
+            cross_symbol.SetActive(false);
         }
         if(joystick_fire.Horizontal==0 && joystick_fire.Vertical == 0 && atisyapildi==1)
         {
@@ -46,7 +91,12 @@ public class movement : MonoBehaviour
             mermi.transform.position = konum;
             Rigidbody2D rigofbullet;
             rigofbullet = mermi.GetComponent<Rigidbody2D>();
-            rigofbullet.velocity = new Vector2(lastfirecoordinates.x * bullet_speed, lastfirecoordinates.y * bullet_speed);
+            float x = lastfirecoordinates.x;
+            float y = lastfirecoordinates.y;
+            float hipo = Mathf.Sqrt(x * x + y * y);
+            float ratio = bullet_speed / hipo;
+
+            rigofbullet.velocity = new Vector2(x*ratio, y*ratio);
         }
     }
 }
