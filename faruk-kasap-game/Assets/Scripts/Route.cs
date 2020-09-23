acb05edb5595683a432418fcf176849534ed8af7
Faruk Avcı <farukkastamonuda@gmail.com>
2020-09-23 15:54:14 +0300

    asdasd
    asdasd

---
+++ faruk-kasap-game/Assets/Scripts/Route.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+    public enum MoveType
+    {
+        Line
+       // Circle
+    }
+    [Serializable]
+    public struct Moves
+    {
+        public MoveType movetype;
+        public Vector2 addition;
+    };
+
+    public List<Moves> moves;
+    public float speed;
+
+    int move = -1;
+    Vector2 startPoint;
+    float dx;
+    float dy;
+    float dist;
+    float ratio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moves[move].movetype == MoveType.Line)
+        {
+
+            transform.position = new Vector3(transform.position.x + dx * ratio * Time.deltaTime
+                , transform.position.y + dy * ratio * Time.deltaTime, 0);
+
+            if (distance(new Vector2(transform.position.x, transform.position.y),
+                new Vector2(startPoint.x + dx, startPoint.y + dy)) <= speed * Time.deltaTime)
+            {
+                transform.position = new Vector3(startPoint.x + dx, startPoint.y + dy, 0);
+                nextMove();
+            }
+        }
+
+    }
+
+    public void nextMove()
+    {
+
+        move = (move + 1) % moves.Count;
+        dx = moves[move].addition.x;
+        dy = moves[move].addition.y;
+        dist = distance(dx, dy);
+        ratio = speed / dist;
+        startPoint = new Vector2(transform.position.x, transform.position.y);
+
+    }
+
+    public float distance(float a , float b)
+    {
+        return Mathf.Sqrt(a*a + b*b);
+    }
+    public float distance(Vector2 a, Vector2 b)
+    {
+        return Mathf.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    }
+}
---
+++ faruk-kasap-game/Assets/Scripts/movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject dayi,bullet;
+    public Joystick joystick_mov,joystick_fire;
+    public float speed,bullet_speed;
+
+    private Rigidbody2D rigbody;
+    private int direction = 0,atisyapildi=0;//0 mean left 1 mean right
+    private Vector2 lastfirecoordinates;
+    void Start()
+    {
+        atisyapildi = direction = 0;
+        rigbody = dayi.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigbody.velocity = new Vector2(speed * joystick_mov.Horizontal, speed * joystick_mov.Vertical);
+        if (joystick_mov.Horizontal > 0 && direction==0)
+        {
+            direction = 1;
+            rigbody.transform.Rotate(0, 180 ,0);
+        }
+        if (joystick_mov.Horizontal <0 && direction == 1)
+        {
+            direction = 0;
+            rigbody.transform.Rotate(0, 180, 0);
+        }
+        if(joystick_fire.Horizontal!=0 || joystick_fire.Vertical != 0)
+        {
+            atisyapildi = 1;
+            lastfirecoordinates = new Vector2(joystick_fire.Horizontal, joystick_fire.Vertical);
+        }
+        if(joystick_fire.Horizontal==0 && joystick_fire.Vertical == 0 && atisyapildi==1)
+        {
+            atisyapildi = 0;
+            GameObject mermi;
+            Vector2 konum = rigbody.transform.position; ;
+            mermi=Instantiate(bullet) as GameObject;
+            mermi.transform.position = konum;
+            Rigidbody2D rigofbullet;
+            rigofbullet = mermi.GetComponent<Rigidbody2D>();
+            rigofbullet.velocity = new Vector2(lastfirecoordinates.x * bullet_speed, lastfirecoordinates.y * bullet_speed);
+        }
+    }
+}
