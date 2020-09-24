3be32f1e9f6cb35d9f8274d60eaa37cec847f376
Faruk Avcı <farukkastamonuda@gmail.com>
2020-09-24 11:00:37 +0300

    kasap
    kasap

---
+++ faruk-kasap-game/Assets/main_camera_hareket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_camera_hareket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 difference_with_dayi;
+    public GameObject camera37;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera37.transform.position = FindObjectOfType<movement>().dayi.transform.position + difference_with_dayi;
+    }
+}
