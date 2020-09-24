e018cb5279ec6bf026feafc2f8925aad4dbc8c3d
Halil-Ibrahim-Kasapoglu <36034222+Halil-Ibrahim-Kasapoglu@users.noreply.github.com>
2020-09-24 08:02:47 +0300

    level scene added
    sadasds

---
+++ faruk-kasap-game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public GameObject LosePanel;
+    public GameObject WinPanel;
+
+    public int scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LosePanel.SetActive(false);
+        //WinPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        if (scene == 8)
+        {
+            MainMenu();
+        }else
+        {
+            scene += 1;
+            PlayerPrefs.SetInt("level" + scene, 1);
+            SceneManager.LoadScene("Level" + scene);
+
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Level" + scene);
+
+    }
+    public void MissionFailed()
+    {
+        LosePanel.SetActive(true);
+    }
+    public void MissionCompleted()
+    {
+        WinPanel.SetActive(true);
+    }
+}
